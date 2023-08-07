b8c9658cc290fe7980f9b12221f1bb53089d3717
MattSun <mattmsun@gmail.com>
2023-08-07 13:05:19 +1000

    nginx default

--- index.js
+++ index.js
@@ -12,26 +12,22 @@ require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 
-// console.log(process.env.NODE_ENV);
-// if (process.env.NODE_ENV === "production") {
-//   https
-//     .createServer(
-//       {
-//         key: fs.readFileSync(path.join(__dirname, "cert/cert.key")),
-//         cert: fs.readFileSync(path.join(__dirname, "cert/cert.pem")),
-//       },
-//       app
-//     )
-//     .listen(80, () => {
-//       console.log(`listening on port 80`);
-//     });
-// } else {
-//   const port = process.env.PORT || 3000;
-//   app.listen(port, () => {
-//     console.log(`listening on port ${port}`);
-//   });
-// }
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
-});
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === "production") {
+  https
+    .createServer(
+      {
+        key: fs.readFileSync(path.join(__dirname, "cert/cert.key")),
+        cert: fs.readFileSync(path.join(__dirname, "cert/cert.pem")),
+      },
+      app
+    )
+    .listen(80, () => {
+      console.log(`listening on port 80`);
+    });
+} else {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+  });
+}
